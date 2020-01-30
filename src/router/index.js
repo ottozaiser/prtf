41c5b9f3ef1e03cdb6803575d45a5453fd617b0c
Otto <zaiser@gmail.com>
2020-01-30 11:47:46 -0300

    Error 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,74 +6,80 @@ import { wrapRouter } from "oaf-vue-router";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home,
-    meta: {
-      title: "Otto Zaiser [Designer]"
-    }
-  },
-  {
-    path: "/portfolio",
-    name: "portfolio",
-    // route level code-splitting
-    // this generates a separate chunk (resume.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "portfolio" */ "../views/Portfolio.vue"),
-    meta: {
-      title: "Portfolio - Otto Zaiser [Designer]"
-    }
-  },
-  // {
-  //   path: '/portfolio/proyecto/:id',
-  //   name: 'description',
-  //   props: true,
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/Description.vue'),
-  //   meta: {
-  //     title: ':id'
-  //   }
-  // },
-  {
-    path: "/resume",
-    name: "resume",
-    // route level code-splitting
-    // this generates a separate chunk (resume.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "resume" */ "../views/Resume.vue"),
-    meta: {
-      title: "CV / Resumé - Otto Zaiser [Designer]"
-    }
-  },
-  {
-    path: "/story",
-    name: "story",
-    props: true,
-    component: () =>
-      import(/* webpackChunkName: "story" */ "../views/Story.vue"),
-    meta: {
-      title: "My Story - Otto Zaiser [Designer]"
-    }
-  }
+	{
+		path: "/",
+		name: "home",
+		component: Home,
+		meta: {
+			title: "Otto Zaiser [Designer]"
+		}
+	},
+	{
+		path: "/portfolio",
+		name: "portfolio",
+		// route level code-splitting
+		// this generates a separate chunk (resume.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "portfolio" */ "../views/Portfolio.vue"),
+		meta: {
+			title: "Portfolio - Otto Zaiser [Designer]"
+		}
+	},
+	// {
+	//   path: '/portfolio/proyecto/:id',
+	//   name: 'description',
+	//   props: true,
+	//   component: () => import(/* webpackChunkName: "about" */ '../views/Description.vue'),
+	//   meta: {
+	//     title: ':id'
+	//   }
+	// },
+	{
+		path: "/resume",
+		name: "resume",
+		// route level code-splitting
+		// this generates a separate chunk (resume.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "resume" */ "../views/Resume.vue"),
+		meta: {
+			title: "CV / Resumé - Otto Zaiser [Designer]"
+		}
+	},
+	{
+		path: "/story",
+		name: "story",
+		props: true,
+		component: () => import(/* webpackChunkName: "story" */ "../views/Story.vue"),
+		meta: {
+			title: "My Story - Otto Zaiser [Designer]"
+		}
+	},
+	{
+		// will match everything
+		path: "*",
+		name: "error404",
+		component: () => import(/* webpackChunkName: "error404" */ "../views/Error404.vue"),
+		meta: {
+			title: "404 - Page not found"
+		}
+	}
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  scrollBehavior() {
-    return { x: 0, y: 0 };
-  },
-  linkActiveClass: "is-active",
-  base: process.env.BASE_URL,
-  routes
+	mode: "history",
+	scrollBehavior() {
+		return { x: 0, y: 0 };
+	},
+	linkActiveClass: "is-active",
+	base: process.env.BASE_URL,
+	routes
 });
 
 wrapRouter(router);
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title;
-  next();
+	document.title = to.meta.title;
+	next();
 });
 
 export default router;
---
+++ src/views/Error404.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="error404" v-if="this.homejson">
+    <div class="layout">
+      <progressive-img class="image" src="/images/error.png" alt />
+      <div class="content">
+        <h1>Error 404</h1>
+        <p>This shitty vuerouter couldn't find this page. Try these instead:</p>
+        <ul class="menu">
+          <li class="menu-item" v-bind:key="index" v-for="(item, index) in this.homejson.links">
+            <router-link :to="item.url" v-if="item.url.startsWith('/')">{{ item.name }}</router-link>
+            <a :href="item.url" target="_blank" v-else>{{ item.name }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "error404",
+  data: function() {
+    return {
+      homejson: null
+    };
+  },
+  created: function() {
+    axios
+      .get("/_data/home.json")
+      .then(response => {
+        this.homejson = response.data;
+      })
+      .catch(error => {
+        alert(error);
+      });
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.error404 {
+  display: flex;
+  flex: 1;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 0 24px;
+  @media (max-width: 640px) {
+    padding: 24px;
+  }
+}
+
+.layout {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  z-index: 2;
+  @media (max-width: 640px) {
+    display: block;
+    text-align: center;
+    flex-direction: row;
+    min-height: auto;
+  }
+}
+.content {
+  max-width: 256px;
+
+  @media (max-width: 640px) {
+    margin: 0 auto;
+    h1 {
+      display: inline-block;
+      &:after {
+        left: 0px;
+      }
+    }
+  }
+  @media (max-width: 300px) {
+    h1:after {
+      content: none;
+    }
+  }
+}
+
+ul.menu {
+  padding: 0;
+  li {
+    display: block;
+    margin: 8px 0;
+    padding: 8px;
+    a {
+      color: black;
+    }
+  }
+  @media (max-width: 640px) {
+    text-align: center;
+  }
+}
+.image {
+  width: 256px;
+  max-width: 256px;
+  max-height: 256px;
+  @media (max-width: 300px) {
+    width: 100%;
+  }
+}
+</style>
